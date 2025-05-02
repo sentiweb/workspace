e8fd0f19133e3b029ffa3fa3ce697da87cfe0a32
Clément Turbelin <clement.turbelin@upmc.fr>
2025-05-02 17:10:38 +0200

    fix function call

--- R/zzz.R
+++ R/zzz.R
@@ -10,17 +10,17 @@
   }
 }
 
-# Default behaviour when attaching workspace (using library(workspace)) is to load the current workspace.
+# Default behaviour when attaching workspace (using library(workspace))
 .onAttach <- function(libname, pkgname) {
   if(pkgname == "workspace") {
     autoload = base::getOption(OPTION_AUTOLOAD, default=FALSE)
     envir = base::getOption(OPTION_ENV, default=.GlobalEnv)
     if(is.logical(autoload)) {
       if( isTRUE(autoload) ) {
-        load_workspace(envir=envir)
+        launch(envir=envir)
       }
     } else {
-      rlang::warning(paste("option ", sQuote(OPTION_AUTOLOAD)," must be logical, value ignored"))
+      rlang::warn(paste("option ", sQuote(OPTION_AUTOLOAD)," must be logical, value ignored"))
     }
   }
 }
